470ffd84bd2bf06c426d88ec83eb8af856c33048
Mayanka11y <mayankraj418890@gmail.com>
2025-03-18 22:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the input element by its id
+const inputField = document.getElementById("fname");
+
+// Add an event listener for the 'blur' event
+inputField.addEventListener("blur", function() {
+    // Convert the input value to uppercase
+    inputField.value = inputField.value.toUpperCase();
+});
